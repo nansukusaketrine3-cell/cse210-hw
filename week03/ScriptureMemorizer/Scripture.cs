fe4135c11d07f877231aef3a31ed2c7a5b2f12f1
nansukusaketrine3-cell <nansukusaketrine3@gmail.com>
2026-07-20 12:30:20 +0300

    commit

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] pieces = text.Split(' ');
+
+        foreach (string word in pieces)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        int count = Math.Min(numberToHide, visibleWords.Count);
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+
+            visibleWords[index].Hide();
+
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = "";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
